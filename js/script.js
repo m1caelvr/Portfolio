fcd09595f90458fa8d8af2d917c23c9a4583e5e2
m1caelvr <134837371+m1caelvr@users.noreply.github.com>
2024-05-21 19:43:55 +0000

    version 8

--- js/script.js
+++ js/script.js
@@ -1,48 +1,70 @@
-const main = document.getElementById('main');
-const sections = document.querySelectorAll('.section');
+const main = document.getElementById("main");
+const sections = document.querySelectorAll(".section");
 
-main.addEventListener('scroll', checkSectionInView);
-window.addEventListener('load', checkSectionInView);
+main.addEventListener("scroll", checkSectionInView);
+window.addEventListener("load", checkSectionInView);
 
 function checkSectionInView() {
-  // console.log("foi");
   const scrollPosition = main.scrollTop;
-  const windowHeight = main.clientHeight;
 
-  sections.forEach(section => {
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionBottom = sectionTop + section.offsetHeight;
 
     if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-      section.classList.add('active');
-      observeFunction(section.id)
+      section.classList.add("active");
+      observeFunction(section.id);
     } else {
-      section.classList.remove('active');
+      section.classList.remove("active");
     }
   });
 }
 
 function observeFunction(i) {
-  const homeIcon = document.getElementById('home-icon');
-  const chevronIcon = document.getElementById('chevron-icon');
-
-  if (i === 'section-landing') {
-    homeIcon.src = './svg/home.svg';
-    if (chevronIcon.classList.contains('chevron-icon-focus')) {
-      chevronIcon.classList.remove('chevron-icon-focus')
-      chevronIcon.src = './svg/chevron-down-circle-outline.svg';
+  const chevronIcon = document.getElementById("chevronIcon");
+  if (i === "section-landing") {
+    toggleFill("homeIcon", "fill");
+    alterarFillIcon("notFill");
+    if (chevronIcon.classList.contains("chevron-icon-focus")) {
+      chevronIcon.classList.remove("chevron-icon-focus");
     }
-  } else if (i === 'section-main') {
-    chevronIcon.classList.add('chevron-icon-focus')
-    homeIcon.src = './svg/home-outline.svg';
-    setTimeout(() => {
-      chevronIcon.src = './svg/chevron-down-circle.svg';
-    }, 250);
+  } else if (i === "section-main") {
+    chevronIcon.classList.add("chevron-icon-focus");
+    toggleFill("homeIcon", "notFill");
+    alterarFillIcon("fill");
+  }
+}
+const actived = document.querySelectorAll(".active");
+actived.forEach((active) => {
+  observeFunction(active);
+});
+
+function alterarFillIcon(i) {
+  var svg = document.getElementById("chevronIcon");
+  var paths = svg.querySelectorAll("path");
+
+  if (i === "fill") {
+    paths[0].setAttribute("fill", "");
+    paths[1].setAttribute("fill", "none");
+    paths[1].setAttribute("stroke", "#000");
+  } else if (i === "notFill") {
+    paths[0].setAttribute("fill", "none");
+    paths[1].setAttribute("fill", "none");
+    paths[1].setAttribute("stroke", "#fafafa");
+  } else {
+    console.error("Ação não reconhecida.");
   }
 }
 
-// Adicionando evento de observação para cada elemento ativo
-const actived = document.querySelectorAll(".active");
-actived.forEach(active => {
-  observeFunction(active);
-});
+function toggleFill(svgId, mode) {
+  const svg = document.getElementById(svgId);
+  const paths = svg.querySelectorAll("path");
+  paths.forEach((path) => {
+    if (mode === "fill") {
+      path.setAttribute("fill", "currentColor");
+    } else if (mode === "notFill") {
+      path.removeAttribute("fill");
+    }
+  });
+}
+toggleFill("homeIcon", "fill");
