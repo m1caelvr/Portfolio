8494b69a5c47b631b5b706a87f8060ea96180b46
m1caelvr <134837371+m1caelvr@users.noreply.github.com>
2024-05-25 22:10:16 +0000

    version 12

--- js/script.js
+++ js/script.js
@@ -1,57 +1,21 @@
-// function observarPosicaoSectionLanding() {
-//   const sectionLanding = document.getElementById('section-landing');
+const sectionMain = document.getElementById('section-main');
+const sections_in_sections_main = document.querySelectorAll('section#section-main > section');
+const nav_section_main_svgs = document.querySelectorAll('#nav-section-main > svg');
+const main = document.getElementById('main');
 
-//   const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         console.log('A seção está dentro da janela.');
-//         // Adicione suas ações aqui quando a seção estiver visível
-//       } else {
-//         console.log('A seção está fora da janela.');
-//         // Adicione suas ações aqui quando a seção estiver fora de vista
-//       }
-//     });
-//   }, { threshold: 0 });
+main.addEventListener('scroll', function () {
+  checkSectionInView();
+});
+sectionMain.addEventListener('scroll', function () {
+  checkSecundarySection();
+});
 
-//   observer.observe(sectionLanding);
-// }
-// observarPosicaoSectionLanding();
-
-// function detectarSeccaoEmFoco() {
-//   const secoes = document.querySelectorAll('main > section');
-//   const mainElement = document.getElementById('main');
-//   console.log("Foi");
-
-//   function verificarSeccaoEmFoco() {
-//     let seccaoEmFoco = null;
-
-//     secoes.forEach(secao => {
-//       const retangulo = secao.getBoundingClientRect();
-//       if (
-//         retangulo.top >= 0 &&
-//         retangulo.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-//       ) {
-//         seccaoEmFoco = secao;
-//       }
-//     });
-
-//     if (seccaoEmFoco) {
-//       const sectionInFocus = seccaoEmFoco.id;
-//       observeFunction(sectionInFocus);
-//       console.log('Seção em foco:', sectionInFocus);
-//     }
-//   }
-
-//   verificarSeccaoEmFoco();
-// }
-
-const sections = document.querySelectorAll('main > section');
+checkSectionInView();
 function checkSectionInView() {
-  console.log("ooo");
-  const main = document.getElementById('main');
+  const sections_in_sections_main = document.querySelectorAll('main > section');
   const scrollPosition = main.scrollTop;
 
-  sections.forEach((section) => {
+  sections_in_sections_main.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionBottom = sectionTop + section.offsetHeight;
 
@@ -64,72 +28,127 @@ function checkSectionInView() {
   });
 }
 
-const main = document.getElementById('main');
-main.addEventListener('scroll', function () {
-  checkSectionInView();
-});
-checkSectionInView();
+function checkSecundarySection() {
+  const sectionMain = document.getElementById('section-main');
+  const scrollPosition = sectionMain.scrollTop + window.innerHeight * 1.2;
 
-function observeFunction(i) {
-  const chevronIcon = document.getElementById("chevronIcon");
-  if (i === "section-landing") {
-    toggleFill("homeIcon", "fill");
-    alterarFillIcon("notFill");
-    headerAdjust("show");
-    if (chevronIcon.classList.contains("chevron-icon-focus")) {
-      chevronIcon.classList.remove("chevron-icon-focus");
+  sections_in_sections_main.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionBottom = sectionTop + section.offsetHeight;
+    if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+      iconSecundarySectionInFocus(section);
+      // console.log(section.className);
+      // alterIconSecundarySection();
     }
-  } else if (i === "section-main") {
-    chevronIcon.classList.add("chevron-icon-focus");
-    toggleFill("homeIcon", "notFill");
-    alterarFillIcon("fill");
-    headerAdjust("noShow");
+  });
+
+  function iconSecundarySectionInFocus(SecundarySectionInFocus) {
+    const firstSection = nav_section_main_svgs[0];
+    const secondSection = nav_section_main_svgs[1];
+    const thirdSection = nav_section_main_svgs[2];
+    const fourthSection = nav_section_main_svgs[3];
+  
+    nav_section_main_svgs.forEach(section => section.classList.remove('focus'));
+  
+    if (SecundarySectionInFocus.classList.contains('about')) {
+      firstSection.classList.add('focus');
+    }
+    if (SecundarySectionInFocus.classList.contains('skills')) {
+      secondSection.classList.add('focus');
+    }
+    if (SecundarySectionInFocus.classList.contains('projects')) {
+      thirdSection.classList.add('focus');
+    }
+    if (SecundarySectionInFocus.classList.contains('contact')) {
+      fourthSection.classList.add('focus');
+    }
   }
 }
+
 const actived = document.querySelectorAll(".active");
 actived.forEach((active) => {
   observeFunction(active);
 });
+function observeFunction(sectionId) {
+  const chevronIcon = document.getElementById("chevronIcon");
+  if (sectionId === "section-landing") {
+    alterIcon_home("homeIcon", "fill");
+    alterIcon_chevronIcon("notFill");
+    headerAdjust("show");
+    SecundarySection("noShow");
+    if (chevronIcon.classList.contains("chevron-icon-focus")) {
+      chevronIcon.classList.remove("chevron-icon-focus");
+    }
+  } else if (sectionId === "section-main") {
+    console.log(sections_in_sections_main.length);
+    chevronIcon.classList.add("chevron-icon-focus");
+    alterIcon_home("homeIcon", "notFill");
+    alterIcon_chevronIcon("fill");
+    headerAdjust("noShow");
+    SecundarySection("show");
+  }
 
-function alterarFillIcon(i) {
-  var svg = document.getElementById("chevronIcon");
-  var paths = svg.querySelectorAll("path");
+  function alterIcon_chevronIcon(sectionId) {
+    var svg = document.getElementById("chevronIcon");
+    var paths = svg.querySelectorAll("path");
+  
+    if (sectionId === "fill") {
+      paths[0].setAttribute("fill", "");
+      paths[1].setAttribute("fill", "none");
+      paths[1].setAttribute("stroke", "#000");
+    } else if (sectionId === "notFill") {
+      paths[0].setAttribute("fill", "none");
+      paths[1].setAttribute("fill", "none");
+      paths[1].setAttribute("stroke", "#fafafa");
+    } else {
+      console.error("Ação não reconhecida.");
+    }
+  }
+  
+  function alterIcon_home(svgId, mode) {
+    nav_section_main_svgs.forEach(svg => {
+      svg.classList.remove('visible');
+    });
 
-  if (i === "fill") {
-    paths[0].setAttribute("fill", "");
-    paths[1].setAttribute("fill", "none");
-    paths[1].setAttribute("stroke", "#000");
-  } else if (i === "notFill") {
-    paths[0].setAttribute("fill", "none");
-    paths[1].setAttribute("fill", "none");
-    paths[1].setAttribute("stroke", "#fafafa");
-  } else {
-    console.error("Ação não reconhecida.");
+    const svg = document.getElementById(svgId);
+    const paths = svg.querySelectorAll("path");
+    paths.forEach((path) => {
+      if (mode === "fill") {
+        path.setAttribute("fill", "currentColor");
+      } else if (mode === "notFill") {
+        path.removeAttribute("fill");
+      }
+    });
+  }
+
+  function SecundarySection(show_icon) {
+    const firstSection = nav_section_main_svgs[0];
+    nav_section_main_svgs.forEach(section => section.classList.remove('focus'));
+    
+    firstSection.classList.add('focus');
+    if (show_icon === 'show') {      
+      nav_section_main_svgs.forEach(svg => {
+        svg.classList.add('visible');
+      });
+    }
+    // else {
+    //   nav_section_main_svgs.forEach(svg => {
+    //     svg.classList.remove('visible');
+    //   });
+    // }
+  }
+  function headerAdjust(show) {
+    const header = document.getElementById("header");
+  
+    if (show === "noShow") {
+      if (!header.classList.contains("header-no-show")) {
+        header.classList.add("header-no-show");
+      }
+    } else {
+      if (header.classList.contains("header-no-show")) {
+        header.classList.remove("header-no-show");
+      }
+    }
   }
 }
 
-function toggleFill(svgId, mode) {
-  const svg = document.getElementById(svgId);
-  const paths = svg.querySelectorAll("path");
-  paths.forEach((path) => {
-    if (mode === "fill") {
-      path.setAttribute("fill", "currentColor");
-    } else if (mode === "notFill") {
-      path.removeAttribute("fill");
-    }
-  });
-}
-
-function headerAdjust(i) {
-  const header = document.getElementById("header");
-
-  if (i === "noShow") {
-    if (!header.classList.contains("header-no-show")) {
-      header.classList.add("header-no-show");
-    }
-  } else {
-    if (header.classList.contains("header-no-show")) {
-      header.classList.remove("header-no-show");
-    }
-  }
-}
