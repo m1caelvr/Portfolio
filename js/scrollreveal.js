24bcefd52e93e185cb6ce34b4fce1516e1d23dd5
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-06-04 02:02:17 -0300

    Scroll Version

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,3 +1,32 @@
-ScrollReveal().reveal('.headline', { interval: 16, reset: true });
-ScrollReveal().reveal('.window', { delay: 700, reset: true });
-// ScrollReveal().reveal('.itens-section', { delay: 700, reset: true });
+const addIntersectionObserver = (elements, delay = 0) => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry, index) => {
+            if (entry.isIntersecting) {
+                if (delay > 0) {
+                    setTimeout(() => {
+                        entry.target.classList.add('show');
+                    }, delay * index);
+                } else {
+                    entry.target.classList.add('show');
+                }
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    elements.forEach((el) => observer.observe(el));
+};
+
+const sectionsToReveal = [
+    { selector: '.landing-reveal', delay: 250 },
+    { selector: '.opacity-reveal', delay: 100 },
+    { selector: '.scale-reveal', delay: 150 },
+    { selector: '.skills-reveal', delay: 150 },
+    { selector: '.itens-section', delay: 150 }
+];
+
+sectionsToReveal.forEach(section => {
+    const elements = document.querySelectorAll(section.selector);
+    addIntersectionObserver(elements, section.delay);
+});
