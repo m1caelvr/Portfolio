1a023135b6ff1f792f3dc17c7db42f05da5a5746
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-06-11 16:00:06 -0300

    ios update

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,4 +1,4 @@
-const addIntersectionObserver = (elements, delay = 0) => {
+const addIntersectionObserver = (elements, delay = 0, threshold = 0) => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry, index) => {
             if (entry.isIntersecting) {
@@ -13,22 +13,22 @@ const addIntersectionObserver = (elements, delay = 0) => {
                 entry.target.classList.remove('show');
             }
         });
-    });
+    }, { threshold });
 
     elements.forEach((el) => observer.observe(el));
 };
 
 const sectionsToReveal = [
-    { selector: '.landing-reveal', delay: 250 },
-    { selector: '.opacity-reveal-delay', delay: 100 },
-    { selector: '.opacity-reveal', delay: 0 },
-    { selector: '.scale-reveal-footer', delay: 100 },
-    { selector: '.scale-reveal', delay: 150 },
-    { selector: '.skills-reveal', delay: 0 },
-    { selector: '.itens-section', delay: 100 }
+    { selector: '.landing-reveal', delay: 250, threshold: 0 },
+    { selector: '.opacity-reveal-delay', delay: 100, threshold: 0 },
+    { selector: '.opacity-reveal', delay: 0, threshold: 0 },
+    { selector: '.scale-reveal-footer', delay: 100, threshold: 0 },
+    { selector: '.scale-reveal', delay: 200, threshold: 0 },
+    { selector: '.skills-reveal', delay: 0, threshold: 0 },
+    { selector: '.itens-section', delay: 100, threshold: 0.3 }
 ];
 
 sectionsToReveal.forEach(section => {
     const elements = document.querySelectorAll(section.selector);
-    addIntersectionObserver(elements, section.delay);
+    addIntersectionObserver(elements, section.delay, section.threshold);
 });
